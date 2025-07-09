cf2bf702198d6445184f5ae7c95e6836e83ba24d
richeevorah <richeevorah@gmail.com>
2025-07-09 13:13:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 60 },
+    size: { value: 3 },
+    color: { value: "#ffffff" },
+    line_linked: { enable: true, color: "#ffffff" },
+    move: { enable: true, speed: 2 }
+  }
+});
